f554f131eda29a8dec23951bec5261bd09da38ee
Marcelo Pierre <marcelo.r.pierre@gmail.com>
2019-11-27 15:18:55 -0300

    Validadores padrões do Mongoose

--- server/error.handler.ts
+++ server/error.handler.ts
@@ -13,8 +13,16 @@ export const handleError = (req: restify.Request, resp: restify.Response, err, d
             if (err.code == 11000)
                 err.statusCode = 400
             break;
-        case 'VAlidationError':
+        case 'ValidationError':
             err.statusCode = 400
+            const messages: any[] = []
+
+            for (let name in err.errors) {
+                messages.push({ message: err.errors[name].message })
+            }
+            err.toJSON = () => ({
+                errors: messages
+            })
             break;
     }
     done()
