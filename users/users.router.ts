44da864d7ae199bdaec20a838974db7a6223c318
Marcelo Pierre <marcelo.r.pierre@gmail.com>
2019-12-13 08:09:41 -0300

    Validações e Middleware em Update (Mongoose)

--- users/users.router.ts
+++ users/users.router.ts
@@ -34,7 +34,7 @@ class UserRouter extends Router {
         })
 
         application.put('/users/:id', (req, resp, next) => {
-            const options = { overwrite: true }
+            const options = { runValidators: true, overwrite: true }
             User.update({ _id: req.params.id }, req.body, options)
                 .exec().then(result => {
                     if (result.n) {
@@ -47,7 +47,7 @@ class UserRouter extends Router {
         }) 
 
         application.patch('/users/:id', (req, resp, next) => {
-            const options = { new: true }
+            const options = { runValidators: true, new: true }
             User.findByIdAndUpdate(req.params.id, req.body, options)
                 .then(this.render(resp, next))
                 .catch(next)
